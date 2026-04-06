606b590f215f060cc182606eb2f149662262e1de
laoshubaby <laoshubaby@protonmail.com>
2026-04-06 09:29:34 +0900

    refactor: IaC aiming folder structure

---
+++ cdn.py
@@ -0,0 +1,11 @@
+# IaC deployment
+# Because CDN is supported by Cloudflare, this place will follow its documents.
+# https://developers.cloudflare.com/workers/platform/infrastructure-as-code/
+
+pair=[
+    ("towait.laoshubaby.moe","laoshubaby.moe/pages/towait")
+    "vscode.laoshubaby.moe",
+    "openhands.laoshubaby.moe",
+    "osm.laoshubaby.moe",
+    
+]
